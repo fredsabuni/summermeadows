37f7d702d2237c92b2e012a8798e45117473db41
Fred Malack <fredmalack@Freds-Laptop.local>
2024-05-21 19:59:01 +0300

    :hammer: Updated the site

---
+++ include/footer.js
@@ -0,0 +1,165 @@
+class Footer extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+
+        this.innerHTML =  `
+        
+        <!-- Download Deck -->
+        <div class="container-xxl project py-5">
+           <div class="container">
+               <div class="text-center mx-auto mb-3 wow fadeInUp" data-wow-delay="0.1s"> 
+                   <p class="mb-4" style="font-size: 16px; display: flex; justify-content: center; align-items: center; flex-wrap: wrap;">
+                       Interested in exploring more about investing in the Nova Summer Meadows deal?<a href="./assets/NOVA RTP 2680_.pdf" style="max-width: 250px; margin-left: 0px; font-family: 'Montserrat', sans-serif; display: inline-block; text-align: center;" class="py-2 px-4" download="NovaSummerMeadows.pdf" target="_blank"><i class="fas fa-file-pdf"></i> Download Deck
+                       </a>
+                   </p> 
+               </div> 
+           </div>        
+       </div>
+       <!-- Download Deck -->
+   
+   
+       <!-- Appointment Start -->
+       <div class="container-xxl py-5">
+           <div class="container">
+               <div class="row g-5">
+                   <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                       <h4 class="section-title">Get In Touch</h4>
+                       <h2 class="display-5 mb-4">First steps towards investing in the Nova Summer Meadows</h2>
+                       <p class="mb-4">Our team is here to answer any questions you may have about our properties, amenities, investment opportunities, or anything else you're curious about.</p>
+                       <div class="row g-4">
+                           <div class="col-12">
+                               <div class="d-flex">
+                                   <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-light" style="width: 65px; height: 65px;">
+                                       <i class="fa fa-2x fa-phone-alt text-primary"></i>
+                                   </div>
+                                   <div class="ms-4">
+                                       <p class="mb-2">Call Us Now</p>
+                                       <h5 style="font-family: 'Montserrat', sans-serif;" class="mb-0">+1 (919) 597-0020</h5>
+                                   </div>
+                               </div>
+                           </div>
+                           <div class="col-12">
+                               <div class="d-flex">
+                                   <div class="d-flex flex-shrink-0 align-items-center justify-content-center bg-light" style="width: 65px; height: 65px;">
+                                       <i class="fa fa-2x fa-envelope-open text-primary"></i>
+                                   </div>
+                                   <div class="ms-4">
+                                       <p class="mb-2">Email Us Now</p>
+                                       <h5 style="font-family: 'Montserrat', sans-serif;" class="mb-0">abraham@abranova.com</h5>
+                                   </div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                       <div class="row g-3">
+                         <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+                         <script>
+                           hbspt.forms.create({
+                             region: "na1",
+                             portalId: "45057484",
+                             formId: "e8dc65eb-81fa-4462-bf65-a5f6f7a004fc"
+                           });
+                         </script>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <!-- Appointment End --> 
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-dark text-body footer mt-5 pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4" style="font-family: 'Montserrat' sans-serif;">Address</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-primary me-3"></i>Abranova Real Estate, LLC. <br/> 16 West Martin St, Suite 301<br/> Raleigh, NC 27601</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt text-primary me-3"></i>+1 (919) 597-0020</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-primary me-3"></i>abraham@abranova.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-square btn-outline-body me-1" href="https://twitter.com/Abranova_Real" target="_blank"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-square btn-outline-body me-1" href="https://m.facebook.com/people/Abraham-Nghwani/100008004229281/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-square btn-outline-body me-1" href="https://www.youtube.com/channel/UCNKfi3iST64Y8Pafpwx2Kfg" target="_blank"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-square btn-outline-body me-0" href="https://www.linkedin.com/in/abranova/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4" style="font-family: 'Montserrat' sans-serif;">Nova Summer Meadows</h4>  
+                    <div class="dropdown">
+                        <a class="btn btn-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Valuation</a> 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="./assets/S&U Nova 2680 Project Value.pdf" download="S&UNova2680ProjectValue.pdf" target="_blank">Independent Valuation</a></li>
+                        <li><a class="dropdown-item" href="./assets/RKW- Lease up Budget Nova Summer Meadows.xlsm" download="BudgetNovaRTP2680.xlsm">Property Management Underwriting</a></li>
+                        <li><a class="dropdown-item" href="./assets/Capital Stack Underwriting.xlsx" download="CapitalStackUnderwriting.xlsx">Capital Stack Underwriting</a></li>  
+                        </ul>
+                    </div>
+                    <div class="dropdown">
+                        <a class="btn btn-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Blueprint</a> 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="https://drive.google.com/file/d/1KRlnIkXV7bo43kNmsttsOGfqV55fTuiZ/view?usp=sharing" download="ArchitectureDrawings.pdf" target="_blank">Architecture Drawings</a></li>
+                        <li><a class="dropdown-item" href="https://drive.google.com/file/d/1-O08qDWUAvwBLdJMBePzq2TgeYxBLZLf/view?usp=sharing" download="CivilDrawings.pdf" target="_blank">Civil Drawings</a></li>  
+                        </ul>
+                    </div>
+                    <div class="dropdown">
+                        <a class="btn btn-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Phase 1 Reports</a> 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="./assets/2624 NC 55.pdf" download="EnvironmentalAssessment.pdf" target="_blank" >Environmental Assessment</a></li>
+                        <li><a class="dropdown-item" href="./assets/2624 NC 55 concept plan 2018-03-16  Apex Concept - Alt 5.pdf" download="SiteReport.pdf" target="_blank">Site Report</a></li>    
+                        </ul>
+                    </div> 
+                    <div class="dropdown">
+                        <a class="btn btn-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">Subscription</a> 
+                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="./assets/Nova Summer Meadows LLC - Operating Agreement.pdf" download="OperatingAgreement.pdf" target="_blank" >Operating Agreement</a></li>
+                        <li><a class="dropdown-item" href="./assets/Nova Summer Meadows LLC - PPM.pdf" download="PPM.pdf" target="_blank">PPM</a></li>  
+                        <li><a class="dropdown-item" href="./assets/Nova Summer Meadows LLC - Subscription Agreement.pdf" download="SubscriptionAgreement.pdf" target="_blank">Subscription Agreement</a></li>  
+                        </ul>
+                    </div>  
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h3 class="text-light mb-4" style="font-family: 'Montserrat' sans-serif;">Quick Links</h3>  
+                    <a class="btn btn-link" href="./summary.html">Project</a>
+                    <a class="btn btn-link" href="./gallery.html">Gallery</a>
+                    <a class="btn btn-link" href="./location.html">Location</a> 
+                    <a class="btn btn-link" href="./contact.html">Contact</a> 
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-light mb-4" style="font-family: 'Montserrat' sans-serif;">Newsletter</h4>
+                    <p>Subscribe to our newsletter</p>
+                    <div class="position-relative mx-auto" style="max-width: 400px;"> 
+                        <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+                        <script>
+                        hbspt.forms.create({
+                            region: "na1",
+                            portalId: "45057484",
+                            formId: "8a71a093-cd95-4390-a7d4-4ebd45936517"
+                        });
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a href="#">Nova at Summer Meadows</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        Designed By <a href="https://abranova.com">Abranova Creatives</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <!-- Footer End --> 
+
+        `;
+    }
+}
+
+customElements.define("footer-element", Footer);
